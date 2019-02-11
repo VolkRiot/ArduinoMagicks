2c57b382097b48eb89e45c266d598c174a762035
Mikhail Metrikin <metrikin@gmail.com>
2019-02-10 20:06:23 -0800

    Buzzer Example

--- DigitalInputs/Digital_Inputs.ino
+++ DigitalInputs/Digital_Inputs.ino
@@ -4,6 +4,7 @@
 int ledPin = 5;
 int buttonApin = 9;
 int buttonBpin = 8;
+bool on = false;
 
 byte leds = 0;
 
@@ -16,6 +17,18 @@ void setup()
 
 void loop() 
 {
+  // Single button set up is here.
+  // if (digitalRead(buttonApin) == LOW) {
+  //   on = !on;
+  // }
+
+  // if (on) {
+  //   digitalWrite(ledPin, HIGH);
+  // } else {
+  //   digitalWrite(ledPin, LOW);
+  // }
+
+  // Two button set up here.
   if (digitalRead(buttonApin) == LOW)
   {
     digitalWrite(ledPin, HIGH);
